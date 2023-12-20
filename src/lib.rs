1942dd240b7201d499088004b01fb19f7f9f8df9
Abdallah Abouabdallah <abdallah.abouabdallah.bmt@gmail.com>
2023-12-20 11:30:00 +0100

    Export modules from lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
 //! RBC Aggregation Analysis
 //!
 //! A toolkit for analyzing red blood cell aggregation from 3D confocal microscopy data.
+
+pub mod mesh;
+pub mod io;
+
+pub use mesh::{Mesh, InertiaProperties};
+pub use io::ObjLoader;
