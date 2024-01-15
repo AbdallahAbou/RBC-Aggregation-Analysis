4b90fa2a03f1c2c257fcfe5fc8334ab56d887651
Abdallah Abouabdallah <abdallah.abouabdallah.bmt@gmail.com>
2024-01-15 10:00:00 +0100

    Add analysis module structure

---
+++ src/analysis/mod.rs
@@ -0,0 +1,7 @@
+//! Analysis algorithms for RBC aggregation studies.
+
+mod pair_correlation;
+mod volume_distribution;
+
+pub use pair_correlation::PairCorrelation;
+pub use volume_distribution::VolumeDistribution;
